8c6da5ae942f91524eb366eb37155d8d0bd62d5c
Viviane Mayra <vivi.mayra01@gmail.com>
2023-01-12 19:41:00 -0300

    Aprimorando os inputs

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -14,12 +14,25 @@ export function Home() {
       <form action="">
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
-          <TaskInput id="task" placeholder="Dê um nome para o seu projeto" />
+          <TaskInput
+            id="task"
+            list="task-suggestions"
+            placeholder="Dê um nome para o seu projeto"
+          />
+
+          <datalist id="task-suggestions">
+            <option value="estudar js" />
+            <option value="ir à academia" />
+            <option value="ler um bom livro" />
+          </datalist>
           <label htmlFor="minutesAmount">Durante</label>
           <MinutesAmountInput
-            typeof="number"
+            type="number"
             id="minutesAmount"
             placeholder="00"
+            step={5}
+            min={5}
+            max={60}
           />
 
           <span>minutos.</span>
