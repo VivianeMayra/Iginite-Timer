8415b0ade3d6a54b8e240a83c075ccbaa8efa033
Viviane Mayra <vivi.mayra01@gmail.com>
2023-01-18 10:45:37 -0300

    Criando contexto entre rotas e resetando o formulário

---
+++ src/pages/Home/components/NewCycleForm/index.tsx
@@ -0,0 +1,41 @@
+import { FormContainer, MinutesAmountInput, TaskInput } from "./styles"
+import { useContext } from "react"
+import { useFormContext } from "react-hook-form"
+import { CyclesContext } from "../../../../contexts/CyclesContext"
+
+export function NewCycleForm() {
+  const { activeCycle } = useContext(CyclesContext)
+  const { register } = useFormContext()
+
+  return (
+    <FormContainer>
+      <label htmlFor="task">Vou trabalhar em</label>
+      <TaskInput
+        id="task"
+        list="task-suggestions"
+        placeholder="Dê um nome para o seu projeto"
+        disabled={!!activeCycle}
+        {...register("task")}
+      />
+
+      <datalist id="task-suggestions">
+        <option value="estudar js" />
+        <option value="ir à academia" />
+        <option value="ler um bom livro" />
+      </datalist>
+      <label htmlFor="minutesAmount">Durante</label>
+      <MinutesAmountInput
+        type="number"
+        id="minutesAmount"
+        placeholder="00"
+        step={1}
+        min={1}
+        max={60}
+        disabled={!!activeCycle}
+        {...register("minutesAmount", { valueAsNumber: true })}
+      />
+
+      <span>minutos.</span>
+    </FormContainer>
+  )
+}
